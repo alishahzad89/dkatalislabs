f97349338afa7cee30c9fc4e1a18ca999dba9468
Shahzad Ali <shahzad@bayzat.com>
2020-08-18 23:52:35 +0530

    Test ng runner class

---
+++ src/test/java/dkatalis/dkatalistest.java
@@ -0,0 +1,18 @@
+package test.java.dkatalis;
+
+import com.dkatalis.api.DataReader;
+import com.dkatalis.api.ResponseValidator;
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
+public class dkatalistest {
+	private Map<String, String> testData;
+	@Test(description = "Compare response of api listed on two files")
+	public void compareResponse() throws Exception {
+		//Read Test data from "testdata.json"
+		testData = DataReader.readData("testdata.json", "CompareResponse");
+		new ResponseValidator().validateResponse(testData.get("FileOneName"), testData.get("FileTwoName"));
+	}
+
+}
